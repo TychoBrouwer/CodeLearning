c24d8477ecd118cb3bd19cb007e403f5cdbe3618
Tycho Brouwer <tychobrouwer33@gmail.com>
2021-09-11 12:30:05 +0200

    auto commit

--- Calculator/dist/public/index.js
+++ Calculator/dist/public/index.js
@@ -1,20 +1,62 @@
-let calculation;
+let calculation = '';
 let currentNumber = '';
 
 const calcMainButtons = document.querySelectorAll('.calcNumberButtons')
+const operatorButtons = document.querySelectorAll('.operatorButtons')
 
 calcMainButtons.forEach(element => {
-  console.log(element)
-
   element.addEventListener("click", function() {
     if (element.getAttribute('dataNumber') !== '.') {
       currentNumber += element.getAttribute('dataNumber')
 
       console.log(currentNumber)
+      console.log(calculation)
     } else if (!currentNumber.includes('.')) {
       currentNumber += element.getAttribute('dataNumber')
 
       console.log(currentNumber)
+      console.log(calculation)
     }
+
+    printCalculation()
   })
 })
+
+operatorButtons.forEach(element => {
+  console.log(element)
+
+  element.addEventListener("click", function() {
+    const operator = element.getAttribute('dataOperator')
+
+    if (operator === 'delete') {
+      if (currentNumber !== '') {
+        currentNumber = currentNumber.slice(0, -1)
+      } else {
+        calculation = calculation.slice(0, -1)
+      }
+    } else if (operator === 'cancel') {
+      currentNumber = '';
+      calculation = '';
+    } else if (operator === 'ans') {
+
+    } else if (operator === '=') {
+
+    } else {
+      calculation += currentNumber + operator
+      currentNumber = ''
+    }
+    
+    console.log(currentNumber)
+    console.log(calculation)
+
+    printCalculation()
+  })
+})
+
+
+function printCalculation() {
+  console.log(currentNumber)
+  console.log(document.getElementById('currentNumber'))
+
+  document.getElementById('currentNumber').innerHTML = currentNumber;
+}
